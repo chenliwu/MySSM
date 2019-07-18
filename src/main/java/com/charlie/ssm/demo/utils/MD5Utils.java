88ab1ef27c2525e8d2737bc3ecb026389453aaea
BYTTER\chenlw <chenlw@bytter.com>
2019-07-18 09:09:58 +0800

    MD5加密

--- src/main/java/com/charlie/ssm/demo/utils/MD5Utils.java
+++ src/main/java/com/charlie/ssm/demo/utils/MD5Utils.java
@@ -7,14 +7,28 @@ import java.security.MessageDigest;
  */
 public class MD5Utils {
 
+
+
+    public static void main(String[]args){
+        try{
+            String testString = "12345678";
+            System.out.println("MD5:"+md5(testString));
+            System.out.println("md5Encode:"+md5Encode(testString.getBytes()));
+        }catch (Exception e){
+            System.out.println("MD5加密失败："+e.getMessage());
+        }
+
+    }
+
+
     /**
      * MD5加密实现
+     *
      * @param str
      * @return
      */
     public static String md5(String str) {
-        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
-                '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         try {
             byte[] strTemp = str.getBytes();
             // 获得MD5摘要算法的 MessageDigest 对象
@@ -37,5 +51,26 @@ public class MD5Utils {
             return null;
         }
     }
+
+    private static final char[] hexadecimal = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    public static String md5Encode(byte[] binaryData) {
+        if (binaryData.length != 16) {
+            return null;
+        } else {
+            char[] buffer = new char[32];
+
+            for(int i = 0; i < 16; ++i) {
+                int low = binaryData[i] & 15;
+                int high = (binaryData[i] & 240) >> 4;
+                buffer[i * 2] = hexadecimal[high];
+                buffer[i * 2 + 1] = hexadecimal[low];
+            }
+
+            return new String(buffer);
+        }
+    }
+
+
 
 }
